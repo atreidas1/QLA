82c04e23506f49be5868c84d08f3bff18e70e50a
Sviataslau Svirydau <sviataslau_svirydau@epam.com>
2017-10-13 14:41:30 +0300

    - Updated linux start script - Updated build.gradle for handling run.sh

--- config/run.sh
+++ config/run.sh
@@ -5,6 +5,6 @@ if [ -z "$JAVA_HOME" ]; then
 fi
 
 export BASE_DIR=`dirname "$0"`
-export JAVA_OPTS="-Dconfig.folder=$BASE_DIR/config -Xms512m -Xmx512m"
+export JAVA_OPTS="-Dconfig.folder=$BASE_DIR/config -Xms128m -Xmx512m -XX:+UseG1GC -XX:+AggressiveOpts"
 $JAVA_HOME/bin/java -jar $JAVA_OPTS $BASE_DIR/QLA-0.0.2-SNAPSHOT.jar
 
